5e9ccbd3612e3b79487bf0a89a1c74f717dac105
Kalugy <dam0171@utp.edu.co>
2024-11-06 18:31:50 -0500

    finish component animals and create new section

--- src/components/searchImg/index.jsx
+++ src/components/searchImg/index.jsx
@@ -2,12 +2,20 @@ import React, { useState } from "react";
 import SearchImg from "./SearchImg";
 
 const App = () => {
+    const [text, setText] = useState();
 
+    const searchImg = async () => {
+        const response = await fetch(`https://picsum.photos/seed/${text}/600/400`) 
+    }
 
     return (
         <div className="h-screen bg-slate-800  text-white flex justify-center items-center">
             <div className="max-w-lg flex flex-col gap-4 mx-auto">
-                <input type="text" className="text-black rounded"/>
+                <input 
+                    type="text" 
+                    className="text-black rounded"
+                    onChange={(e)=> setText(e.target.value)}
+                />
             </div>
         </div>
     )
