0e8f9a4b0600bbd1c5789fa76beec66efeb8d350
Kalugy <dam0171@utp.edu.co>
2024-11-08 17:06:10 -0500

    complete the new section

--- src/components/searchImg/index.jsx
+++ src/components/searchImg/index.jsx
@@ -1,13 +1,23 @@
 import React, { useState } from "react";
-import SearchImg from "./SearchImg";
+//import SearchImg from "./SearchImg";
 
 const App = () => {
     const [text, setText] = useState();
+    const [img, setImg] = useState([]);
 
     const searchImg = async () => {
         const response = await fetch(`https://picsum.photos/seed/${text}/600/400`) 
+        console.log(response.url)
+        return response.url
     }
-
+    const handleKeyDown = async (event) => {
+        if (event.key === 'Enter') {
+          console.log('Enter key pressed:');
+          // Add any additional functionality here, like submitting the input
+          const newImg = searchImg();
+          setImg(...img, newImg)
+        }
+    }
     return (
         <div className="h-screen bg-slate-800  text-white flex justify-center items-center">
             <div className="max-w-lg flex flex-col gap-4 mx-auto">
@@ -15,6 +25,7 @@ const App = () => {
                     type="text" 
                     className="text-black rounded"
                     onChange={(e)=> setText(e.target.value)}
+                    onKeyDown={handleKeyDown}
                 />
             </div>
         </div>
