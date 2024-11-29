69bbc5b837f0ca22ca3d9f531eafd8d48f1c1895
Kalugy <dam0171@utp.edu.co>
2024-11-29 17:31:04 -0500

    finish a search input

--- src/components/registryProject/components/Header.tsx
+++ src/components/registryProject/components/Header.tsx
@@ -1,11 +1,13 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import SearchInput from "./SearchInput"
 
 function Header() {
 
     return (
-      <div className="p-20">
+      <div className="p-20 flex flex-row justify-between ">
         <Link to="/registry">NPM Registry</Link>
+        <SearchInput />
       </div>
     )
   }
--- src/components/registryProject/components/SearchInput.tsx
+++ src/components/registryProject/components/SearchInput.tsx
@@ -1,18 +1,37 @@
 import { useState } from "react";
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Form } from "react-router-dom";
+import { ImSearch } from "react-icons/im";
 
 const SearchInput = () => {
     const [term, setTerm] = useState('')
+    const navigate = useNavigate()
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
-
+        navigate('/registry/search' + '?term=' + term)
     }
 
     return (
     <form onSubmit={handleSubmit}>
-       <input value={term} onChange={(e) => setTerm(e.target.value)} />   
+        <div className="relative">
+            <div className="absolute inset-y-0 flex items-center pl-2">
+                <ImSearch className="h-5 w-5 text-gray-800" />
+            </div>
+            <input placeholder="Search..." className="p-2 pl-8 text-gray-800 italic font-semibold text-md rounded shadow-sm " value={term} onChange={(e) => setTerm(e.target.value)} />   
+    
+        </div>
     </form>
+    )
+}
+
+//example form with router-dom
+const SearchInput2 = () => {
+    return (
+    <Form action="/registry/search">
+       <input placeholder="Search..." className="input" name='term'/>   
+    </Form>
     )
 }
 
